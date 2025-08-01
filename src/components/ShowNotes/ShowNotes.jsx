a350d9c3d9c4285b719cbced744676b777a92a80
Raphael Holzer <info@digitaleweltlibrary.at>
2025-08-01 13:58:28 +0200

    incorrect if statement

--- src/components/ShowNotes/ShowNotes.jsx
+++ src/components/ShowNotes/ShowNotes.jsx
@@ -1,9 +1,16 @@
 import NoNotes from "../NoNotes/NoNotes";
+import NotesList from "../NotesList/NotesList";
 
 export default function ShowNotes({ notes, changeOpen }) {
+  const length = notes.length;
+
   return (
-    <main className={false ? null : "nothing"}>
-      {false ? null : <NoNotes changeOpen={changeOpen} />}
+    <main className={length == 0 ? "nothing" : null}>
+      {length == 0 ? (
+        <NoNotes changeOpen={changeOpen} />
+      ) : (
+        <NotesList notes={notes} />
+      )}
     </main>
   );
 }
