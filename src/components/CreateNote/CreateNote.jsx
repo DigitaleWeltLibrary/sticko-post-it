33671b94247697316e3e14ef974a776ae9e62af0
Raphael Holzer <info@digitaleweltlibrary.at>
2025-08-02 14:56:24 +0200

    Bugfix: While creating or editing a note, you can no longer interact with Notes

--- src/components/CreateNote/CreateNote.jsx
+++ src/components/CreateNote/CreateNote.jsx
@@ -35,47 +35,51 @@ export default function CreateNote({
   };
 
   return (
-    <article className="createnote">
-      <h2>{editID != null ? "Create New Note" : "Edit your Note"}</h2>
-      <p>Write your thoughts and choose a color for your new sticky note.</p>
-      <textarea
-        placeholder="Type your note here"
-        maxLength={200}
-        value={note.text}
-        onChange={(e) => setNote((prev) => ({ ...prev, text: e.target.value }))}
-      />
-      <p className="length">{note.text.length}/200 characters</p>
-      <h2>Color</h2>
-      <section>
-        {colors.map((color) => (
-          <div
-            className={`color ${note.color === color ? "active" : ""}`}
-            key={color}
-            style={{
-              backgroundColor: `hsl(var(${color}))`,
-              border: `4px solid hsl(var(${color}-foreground))`,
-            }}
-            onClick={() => setNote((prev) => ({ ...prev, color }))}
-          ></div>
-        ))}
-      </section>
-      <h2>Preview</h2>
-      <div
-        className="note"
-        style={{ backgroundColor: `hsl(var(${note.color}))` }}
-      >
-        {note.text || "Your note content will appear here..."}
-      </div>
-
-      <div className="btn">
-        <button onClick={() => changeOpen()}>Cancel</button>
-        <button
-          disabled={!note.text}
-          onClick={editID != null ? editnote : addNoteToArray}
+    <article>
+      <article className="createnote">
+        <h2>{editID != null ? "Create New Note" : "Edit your Note"}</h2>
+        <p>Write your thoughts and choose a color for your new sticky note.</p>
+        <textarea
+          placeholder="Type your note here"
+          maxLength={200}
+          value={note.text}
+          onChange={(e) =>
+            setNote((prev) => ({ ...prev, text: e.target.value }))
+          }
+        />
+        <p className="length">{note.text.length}/200 characters</p>
+        <h2>Color</h2>
+        <section>
+          {colors.map((color) => (
+            <div
+              className={`color ${note.color === color ? "active" : ""}`}
+              key={color}
+              style={{
+                backgroundColor: `hsl(var(${color}))`,
+                border: `4px solid hsl(var(${color}-foreground))`,
+              }}
+              onClick={() => setNote((prev) => ({ ...prev, color }))}
+            ></div>
+          ))}
+        </section>
+        <h2>Preview</h2>
+        <div
+          className="note"
+          style={{ backgroundColor: `hsl(var(${note.color}))` }}
         >
-          {editID != null ? "Edit" : "Create Note"}
-        </button>
-      </div>
+          {note.text || "Your note content will appear here..."}
+        </div>
+
+        <div className="btn">
+          <button onClick={() => changeOpen()}>Cancel</button>
+          <button
+            disabled={!note.text}
+            onClick={editID != null ? editnote : addNoteToArray}
+          >
+            {editID != null ? "Edit" : "Create Note"}
+          </button>
+        </div>
+      </article>
     </article>
   );
 }
