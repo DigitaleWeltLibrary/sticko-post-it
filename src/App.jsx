e20bbdd84fa14c7eebe8d2d95b4d93f90fdeffc7
Raphael Holzer <info@digitaleweltlibrary.at>
2025-08-02 14:32:36 +0200

    add fonts

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import CreateNote from "./components/CreateNote/CreateNote";
 import Header from "./components/Header/Header";
 import "./main.scss";
+import "./fonts.scss";
 import ShowNotes from "./components/ShowNotes/ShowNotes";
 
 function App() {
